fe33ee0218c8a2ba419548b778bbe8681ef74ab2
Matt <64214044+kvbc@users.noreply.github.com>
2022-05-21 19:04:11 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-g++ klgr.cpp -o klgr -static -static-libgcc -static-libstdc++
+g++ klgr.cpp -o klgr -static -static-libgcc -static-libstdc++ -s -O2
 pkg protected-text.js -t node16 -o protected-text
